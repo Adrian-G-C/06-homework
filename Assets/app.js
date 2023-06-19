47200ab4e15932c445e254309d8dd76b8c8fedc0
Adrian <121769198+Adi-de-la-Adrian@users.noreply.github.com>
2023-06-19 10:48:05 -0500

    app now displaying icon relative to weather

--- Assets/app.js
+++ Assets/app.js
@@ -70,3 +70,43 @@ function addToSearchHistory(city) {
     getWeatherData(city);
   });
 }
+
+function renderCurrentWeather(currentWeather) {
+    const weatherIconClass = getWeatherIconClass(currentWeather.weather[0].icon);
+  
+    currentWeatherSection.innerHTML = `
+      <h2>Current Weather</h2>
+      <p>City: ${currentWeather.name}</p>
+      <p>Date: ${new Date(currentWeather.dt_txt).toLocaleDateString()}</p>
+      <p><i class="${weatherIconClass}"></i></p>
+      <p>Temperature: ${currentWeather.main.temp} &#8451;</p>
+      <p>Humidity: ${currentWeather.main.humidity}%</p>
+      <p>Wind Speed: ${currentWeather.wind.speed} m/s</p>
+    `;
+  }
+  
+  function getWeatherIconClass(iconCode) {
+    const iconMapping = {
+      '01d': 'fas fa-sun',
+      '01n': 'fas fa-moon',
+      '02d': 'fas fa-cloud-sun',
+      '02n': 'fas fa-cloud-moon',
+      '03d': 'fas fa-cloud',
+      '03n': 'fas fa-cloud',
+      '04d': 'fas fa-cloud',
+      '04n': 'fas fa-cloud',
+      '09d': 'fas fa-cloud-showers-heavy',
+      '09n': 'fas fa-cloud-showers-heavy',
+      '10d': 'fas fa-cloud-rain',
+      '10n': 'fas fa-cloud-rain',
+      '11d': 'fas fa-bolt',
+      '11n': 'fas fa-bolt',
+      '13d': 'fas fa-snowflake',
+      '13n': 'fas fa-snowflake',
+      '50d': 'fas fa-smog',
+      '50n': 'fas fa-smog'
+    };
+  
+    return iconMapping[iconCode] || 'fas fa-question';
+  }
+  
